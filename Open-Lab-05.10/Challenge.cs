0d51967f04aad5d8f916f89bb9f7f65e1b58fa79
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-24 20:59:42 +0100

    done

--- Open-Lab-05.10/Challenge.cs
+++ Open-Lab-05.10/Challenge.cs
@@ -7,6 +7,14 @@ namespace Open_Lab_05._10
         public int MysteryFunc(int num)
         {
             throw new NotImplementedException();
+            int total_number = 1;
+            char[] num_characters = num.ToString().ToCharArray();
+            foreach (var number_char in num_characters)
+            {
+                int intStr; bool intResultTryParse = int.TryParse(number_char.ToString(), out intStr);
+                total_number = total_number * intStr;
+            }
+            return total_number;
         }
     }
 }
